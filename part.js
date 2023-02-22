4cf95c508aae5726e1ee8432b8668c69d9069891
Sheimame <ifechukwudenwafor94@gmail.com>
2023-02-22 15:29:55 +0000

    update

--- part.js
+++ part.js
@@ -2,3 +2,17 @@ function main(){
 	console.log("i am a beginner");
 }
 main();
+
+var age = 19
+console.log(age);
+
+console.log(typeof "16");
+
+console.log(30 + 44);
+console.log(30 * 44);
+console.log(Math.random());
+console.log(Math.PI);
+console.log("50" + 44);
+console.log(parseInt("50") + 44);
+console.log(+"50" + 44);
+
