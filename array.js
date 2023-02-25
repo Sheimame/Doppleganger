905225c9d5ea4422aa73ca4be5564a7a1bdb371b
Sheimame <ifechukwudenwafor94@gmail.com>
2023-02-25 18:12:19 +0000

    update

--- array.js
+++ array.js
@@ -1,3 +1,10 @@
-var myArray = ["age", 9, 8, 7, 6, "this", "that"
-	console.log (myArray.length),
-			
+// var myArray = ['age', 9, 8, 7, 6, 'this', 'that'];
+// console.log(myArray);
+
+var myArray = ['age', 5, 9, 2, 'this', 'that']
+console.log(myArray);
+var myCon = ['sip', 11, true];
+console.log(myCon);
+var myLoc = ['sop', 12, false];
+console.log(myArray.concat(myArray.join('/') + '' + myCon.join('/') + '' + myLoc.join('/')));
+// console.log(myArray.join('/'));
--- function.js
+++ function.js
@@ -1,4 +1,39 @@
-myFuntion : function(){
-	console.log ("firstname" : + this.firstname);
-};
- 	console.log(name);
+// var myObject = {
+// 	firstname: "Ifechukwude", 
+// myFunction : function (){
+// 	console.log ("firstname:" + this.firstname);
+// }
+// };
+// name.myFunction();
+
+// var age =123;
+// age = 'the'
+// console.log(age)
+// var count = 1;
+// var myName = 
+	
+// function hi(){
+// 	console.log(myLastName, '' + myName);
+// }
+// hi();
+// hi();
+
+// var count = 1
+// function increase(){
+// 	count += 5;
+// 	console.log(count);
+// }
+// 	increase();
+// 	increase();
+// 	increase();
+// 	increase();
+
+function hi(){
+	var shouldSayHi = true;
+	var reallyHi = false;
+	var justHi = true;
+	if (shouldSayHi === true){
+		console.log(shouldSayHi);
+	}
+}
+hi();
